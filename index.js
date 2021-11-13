086e76326304e9dcafba809ae79799df6ee818af
ChristianRodarmel <christian.rodarmel1@gmail.com>
2021-11-13 09:32:52 -0800

    Mongoose Part 3

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Campsite = require('./models/campsite');
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -18,10 +19,25 @@ connect.then(() => {
     })
     .then(campsite => {
         console.log(campsite);
-        return Campsite.find();
+        return Campsite.findByIdAndUpdate(campsite._id,{
+            $set: { description: 'Updated Test Document' }
+        }, {
+            new: true
+        });
     })
-    .then(campsites => {
-        console.log(campsites);
+    .then(campsite => {
+        console.log(campsite);
+
+        campsite.comments.push({
+            rating: 5,
+            text: "What a magnificent view!",
+            author: 'Tinus Lorvaldes'
+        });
+
+        return campsite.save();
+    })
+    .then(campsite => {
+        console.log(campsite);
         return Campsite.deleteMany();
     })
     .then(() => {
--- models/campsite.js
+++ models/campsite.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
 const campsiteSchema = new Schema({
     name: {
         type: String,
@@ -10,7 +28,8 @@ const campsiteSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    comments: [commentSchema]
 }, {
     timestamps: true
 });
