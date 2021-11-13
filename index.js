7cf5c2761596c409d168ba313d4a6dbe960c4c78
ChristianRodarmel <christian.rodarmel1@gmail.com>
2021-11-13 09:15:55 -0800

    Mongoose Part 1

---
+++ index.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Campsite = require('./models/campsite');
+
+const url = 'mongodb://localhost:27017/nucampsite';
+const connect = mongoose.connect(url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+connect.then(() => {
+
+    console.log('Connected correctly to server');
+
+    const newCampsite = new Campsite({
+        name: 'React Lake Campground',
+        description: 'test'
+    });
+
+    newCampsite.save()
+    .then(campsite => {
+        console.log(campsite);
+        return Campsite.find();
+    })
+    .then(campsites => {
+        console.log(campsites);
+        return Campsite.deleteMany();
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch(err => {
+        console.log(err);
+        mongoose.connection.close();
+    });
+});
---
+++ models/campsite.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const campsiteSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const Campsite = mongoose.model('Campsite', campsiteSchema);
+
+module.exports = Campsite;
